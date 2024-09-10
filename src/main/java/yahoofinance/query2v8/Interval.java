1255cccfb27a3634c353295a669f38a5038a4103
David Li <davidlislc@users.noreply.github.com>
2024-09-10 00:00:59 -0400

    added more intervals

--- src/main/java/yahoofinance/query2v8/Interval.java
+++ src/main/java/yahoofinance/query2v8/Interval.java
@@ -6,11 +6,19 @@ package yahoofinance.query2v8;
  * @author Stijn Strickx
  */
 public enum Interval {
-    
-    DAILY("d"),
-    WEEKLY("w"),
-    MONTHLY("m");
-    
+
+    DAILY("1d"),
+    WEEKLY("5d"),
+    MONTHLY("1mo"),
+    MINUTE("1m"),
+    TWO_MINUTE("2m"),
+    FIVE_MINUTE("5m"),
+    FIFTEEN_MINUTE("15m"),
+    THIRTY_MINUTE("30m"),
+    SIXTY_MINUTE("60m"),
+    NINETY_MINUTE("90m"),
+    HOURLY("1h"),
+    THREE_MONTH("3mo");
     private final String tag;
     
     Interval(String tag) {
